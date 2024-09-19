b5b5653701dbcdef8cd40c6bb9c0fc008eac220c
BaraAttar <Barraatar@gmail.com>
2024-09-19 23:38:38 +0300

    test api

--- server.js
+++ server.js
@@ -42,6 +42,6 @@ app.use("/category" , categoryRoutes)
 connectMongoDB();
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on port : ${port}`);
 });
  
